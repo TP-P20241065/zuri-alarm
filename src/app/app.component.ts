69b554634526c9d4fa6f9fedd2607f6175ed922d
MaxTer45 <106483409+MaxTer43@users.noreply.github.com>
2024-06-23 14:27:18 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { environment } from '../environments/environment.prod';
 
 @Component({
   selector: 'app-root',
@@ -14,7 +15,30 @@ export class AppComponent {
   private readonly activeImage: string = 'assets/alarm_image2.png';
 
   toggleImage() {
+    if (this.alarmImage == this.activeImage) return;
     this.alarmImage = this.activeImage;
+    const report = {
+      Id: 0,
+      DateTime: new Date().toISOString(), // Asegúrate de tener la fecha y hora correctas
+      Address: "Dentro del bus",
+      Incident: "Alarma en unidad 1",
+      TrackingLink: "https://maps.app.goo.gl/PsJQiVvKxLcFXiEM9",
+      Image: "Imagen"
+    };
+
+    console.log(environment.API_KEY);
+    // Realiza la solicitud POST
+    fetch(environment.API_KEY, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(report),
+    })
+      .then(response => response.json())
+      .then(data => console.log('Success:', data))
+      .catch((error) => console.error('Error:', error));
+
     setTimeout(() => {
       this.alarmImage = this.defaultImage;
     }, 5 * 60 * 1000); // 5 minutos
